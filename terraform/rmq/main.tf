d11620e55f8260ebe3a222e0cfda6b26ee9e95c6
techsysvbo <techsysvbo@gmail.com>
2022-04-10 05:56:26 +0000

    Added files and dir

---
+++ terraform/rmq/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+    region = "us-east-1"
+    profile = var.profile
+}
+
+resource "aws_instance" "rmq" {
+    ami = "ami-07ebfd5b3428b6f4d"
+    instance_type = "t2.micro"
+    key_name = "rabbitmq"
+    vpc_security_group_ids = ["sg-0e2e056a0c9e6abcc"]
+
+    tags = {
+        Name = var.name
+        group = var.group
+    }
+}
---
+++ terraform/rmq/variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+    description = "name the instance on deploy"
+}
+
+variable "group" {
+    description = "The group name that ansible's dynamic inventory will groups"
+}
+
+variable "profile" {
+    description = "Which profile to use for IAM"
+}

e1be3c5e2aae1a9458e25d7a3546d51727535a44
techsysvbo <techsysvbo@gmail.com>
2022-04-10 14:08:01 +0000

    tf files

--- rmq/main.tf
+++ rmq/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "rmq" {
     ami = "ami-07ebfd5b3428b6f4d"
     instance_type = "t2.micro"
     key_name = "rabbitmq"
-    vpc_security_group_ids = ["sg-0e2e056a0c9e6abcc"]
+    vpc_security_group_ids = ["sg-0ebd5caa0e1980399"]
 
     tags = {
         Name = var.name
